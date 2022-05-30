1b4b113a00be086276e670b37f912b758794e32f
tmbuza <ndelly@gmail.com>
2022-05-29 23:24:10 -0500

    Update files

--- R/common.R
+++ R/common.R
@@ -53,11 +53,9 @@ library(magrittr)
 if(!require("scales")) {install.packages("scales")}
 library(scales)
 
-# 
-# if(!require("")) {install.packages("")}
-# library()
-# 
-# 
+if(!require("cgwtools")) {install.packages("cgwtools")}
+library()
+
 # if(!require("")) {install.packages("")}
 # library()
 
